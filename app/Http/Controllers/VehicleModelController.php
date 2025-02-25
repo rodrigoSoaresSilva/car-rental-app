b5a846e6c19ab72fe80a921e2e8f9f4cc2e5f402
Rodrigo Soares da Silva <guigocore@gmail.com>
2025-02-25 14:38:14 -0300

    feat: add VehicleModel model and API CRUD operations

--- app/Http/Controllers/VehicleModelController.php
+++ app/Http/Controllers/VehicleModelController.php
@@ -3,16 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Models\VehicleModel;
+use App\Traits\HandlesModelNotFound;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Storage;
 
 class VehicleModelController extends Controller
 {
+    use HandlesModelNotFound;
+
+    private $vehicle_model;
+    private $uploadImagePath = 'images/vehicle_models';
+    private $notFoundMessage = 'Vehicle Model not found.';
+
+    public function __construct(VehicleModel $vehicle_model){
+        $this->vehicle_model = $vehicle_model;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $vehicle_models = $this->vehicle_model->with('brand')->get();
+
+        return response()->json(
+            [
+                'data' => $vehicle_models
+            ],
+            Response::HTTP_OK
+        );
     }
 
     /**
@@ -20,30 +41,91 @@ class VehicleModelController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->vehicle_model->rules(), $this->vehicle_model->feedback());
+
+        $image = $request->file('image');
+        $imageUrn = $image->store($this->uploadImagePath, 'public');
+
+        $newVehicleModel = [
+            'brand_id' => $request->brand_id,
+            'name' => $request->name,
+            'image' => $imageUrn,
+            'num_doors' => $request->num_doors,
+            'num_seats' => $request->num_seats,
+            'airbag' => $request->airbag,
+            'abs_brakes' => $request->abs_brakes
+        ];
+
+        return response()->json(
+            [
+                'data' => $this->vehicle_model->create($newVehicleModel)
+            ],
+            Response::HTTP_CREATED
+        );
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(VehicleModel $vehicleModel)
+    public function show(int $id)
     {
-        //
+        $vehicle_model = $this->findOrFailWithResponse(VehicleModel::with('brand'), $id, $this->notFoundMessage);
+        
+        if ($vehicle_model instanceof JsonResponse) {
+            return $vehicle_model;
+        }
+
+        return response()->json(['data' => $vehicle_model], Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, VehicleModel $vehicleModel)
+    public function update(Request $request, int $id)
     {
-        //
+        $vehicle_model = $this->findOrFailWithResponse(VehicleModel::class, $id, $this->notFoundMessage);
+        
+        if ($vehicle_model instanceof JsonResponse) {
+            return $vehicle_model;
+        }
+
+        $rules = $request->isMethod('patch')
+                            ? collect($vehicle_model->rules())->only(array_keys($request->all()))->all()
+                            : $vehicle_model->rules();
+
+        $updated_vehicle_model = $request->validate($rules, $this->vehicle_model->feedback());
+
+        if ($request->hasFile('image')) {
+            $oldImage = $vehicle_model->image;
+            $updated_vehicle_model['image'] = $request->file('image')->store($this->uploadImagePath, 'public');
+        }
+        
+        $vehicle_model->update($updated_vehicle_model);
+
+        if (!empty($oldImage)) {
+            Storage::disk('public')->delete($oldImage);
+        }
+
+        return response()->json(['data' => $vehicle_model], Response::HTTP_OK);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(VehicleModel $vehicleModel)
+    public function destroy(int $id)
     {
-        //
+        $vehicle_model = $this->findOrFailWithResponse(VehicleModel::class, $id, $this->notFoundMessage);
+        
+        if ($vehicle_model instanceof JsonResponse) {
+            return $vehicle_model;
+        }
+
+        $oldImage = $vehicle_model->image;
+
+        $vehicle_model->delete();
+
+        Storage::disk('public')->delete($oldImage);
+
+        return response()->noContent();
     }
 }
--- app/Models/VehicleModel.php
+++ app/Models/VehicleModel.php
@@ -6,5 +6,56 @@ use Illuminate\Database\Eloquent\Model;
 
 class VehicleModel extends Model
 {
-    //
+    protected $fillable = ['brand_id', 'name', 'image', 'num_doors', 'num_seats', 'airbag', 'abs_brakes'];
+
+    public function brand(){
+        return $this->belongsTo('App\Models\Brand');
+    }
+
+    /**
+     * Get the validation rules for the model.
+     *
+     * This method defines the validation rules used when creating or updating 
+     * a brand. It ensures that the 'name' field is required and unique, 
+     * and that the 'image' field is required and must be a PNG file.
+     *
+     * @return array The validation rules.
+     */
+    public function rules(): array {
+        return [
+            'brand_id' => 'exists:brands,id',
+            'name' => 'required|unique:vehicle_models,name,'.$this->id.'|min:3',
+            'image' => 'required|file|mimes:png',
+            'num_doors' => 'required|integer|digits_between:1,5',
+            'num_seats' => 'required|integer|digits_between:1,50',
+            'airbag' => 'required|boolean',
+            'abs_brakes' => 'required|boolean'
+        ];
+    }
+
+    /**
+     * Get the validation error messages.
+     *
+     * This method returns an array of custom error messages 
+     * for validation failures. It provides user-friendly messages 
+     * for required fields and unique constraints.
+     *
+     * @return array The validation error messages.
+     */
+    public function feedback(): array {
+        return [
+            'required' => 'The :attribute field is required.',
+            'brand_id.exists' => 'The selected brand does not exist.',
+            'name.unique' => 'The vehicle model name already exists.',
+            'name.min' => 'The vehicle model name must be at least 3 characters long.',
+            'image.file' => 'The image must be a valid file.',
+            'image.mimes' => 'The image must be in PNG format.',
+            'num_doors.integer' => 'The number of doors must be an integer.',
+            'num_doors.digits_between' => 'The number of doors must be between 1 and 5.',
+            'num_seats.integer' => 'The number of seats must be an integer.',
+            'num_seats.digits_between' => 'The number of seats must be between 1 and 50.',
+            'airbag.boolean' => 'The airbag field must be true or false.',
+            'abs_brakes.boolean' => 'The ABS brakes field must be true or false.',
+        ];
+    }
 }
