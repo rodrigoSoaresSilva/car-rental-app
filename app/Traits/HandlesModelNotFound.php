e047d5946b63993544178710c67c2a5f5caa39c8
Rodrigo Soares da Silva <guigocore@gmail.com>
2025-02-25 14:37:50 -0300

    feat: improve findOrFailWithResponse to support Model, string, and Builder

--- app/Traits/HandlesModelNotFound.php
+++ app/Traits/HandlesModelNotFound.php
@@ -2,6 +2,8 @@
 
 namespace App\Traits;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,15 +13,15 @@ trait HandlesModelNotFound
     /**
      * Searches for a model by ID and handles approaches.
      *
-     * @param  string $model   Fully qualified model class name
-     * @param  int    $id      ID of the resource
-     * @param  string $message Custom error message
+     * @param  Model|string|Builder $model   Fully qualified model class name
+     * @param  int                  $id      ID of the resource
+     * @param  string               $message Custom error message
      * @return Model|JsonResponse
      */
-    public function findOrFailWithResponse(string $model, int $id, string $message = 'Resource not found.'): Model|JsonResponse
+    public function findOrFailWithResponse(Model|string|Builder $model, int $id, string $message = 'Resource not found.'): Model|JsonResponse
     {
         try {
-            return $model::findOrFail($id);
+            return is_string($model) ? $model::findOrFail($id) : $model->findOrFail($id);
         } catch (ModelNotFoundException $e) {
             return response()->json(['message' => $message], Response::HTTP_NOT_FOUND);
         }
