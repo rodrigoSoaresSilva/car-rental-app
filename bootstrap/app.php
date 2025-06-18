056f9b1b18f8515ab4badfbfb5be6c27d4e86097
Rodrigo Soares da Silva <guigocore@gmail.com>
2025-06-18 10:02:50 -0300

    feat: secure login routes using middleware and implement AuthController

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'jwt.auth' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
+            'jwt.refresh' => \Tymon\JWTAuth\Http\Middleware\RefreshToken::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/api.php
+++ routes/api.php
@@ -7,16 +7,29 @@ use App\Http\Controllers\VehicleModelController;
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\RentalController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::prefix('v1')->middleware('jwt.auth')->group(function () {
+    Route::apiResources([
+        'brand' => BrandController::class,
+        'vehicle-model' => VehicleModelController::class,
+        'car' => CarController::class,
+        'client' => ClientController::class,
+        'rental' => RentalController::class,
+    ]);
+});
 
-Route::apiResources([
-    'brand' => BrandController::class,
-    'vehicle-model' => VehicleModelController::class,
-    'car' => CarController::class,
-    'client' => ClientController::class,
-    'rental' => RentalController::class,
-]);
+Route::prefix('auth')->group(function () {
+
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::middleware('jwt.auth')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('me', [AuthController::class, 'me']);
+    });
+});
