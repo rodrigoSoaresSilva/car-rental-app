ed88c507735cbbf6ffec6cad9c84eeb080de4cab
Rodrigo Soares da Silva <guigocore@gmail.com>
2025-06-25 09:21:45 -0300

    refactor: remove static content and add dynamic logic to Table component

--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -2,35 +2,26 @@
     <table class="table table-striped table-hover">
         <thead>
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th v-for="title, key in titles" :key="key" scope="col">{{ title.title }}</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            </tr>
-            <tr>
-            <th scope="row">3</th>
-            <td>John</td>
-            <td>Doe</td>
-            <td>@social</td>
+            <tr v-for="(obj, index) in data" :key="index">
+                <td v-for="(value, key) in titles" :key="value.title + '-' + key">
+                    <span v-if="value.type !== 'image'">
+                        {{ obj[key] }}
+                    </span>
+                    <span v-else>
+                        <img :src="'/storage/' + obj[key]" width="30" height="30" alt="logo">
+                    </span>
+                </td>
             </tr>
         </tbody>
     </table>
 </template>
 
 <script>
-    export default {}
+    export default {
+        props: ['data', 'titles']
+    }
 </script>
